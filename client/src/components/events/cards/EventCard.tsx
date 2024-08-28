1ef37faa4d4badcde6f40f382dcd7a8a63df0c91
Abhishek Kashyap <abhik5567@gmail.com>
2024-08-28 17:47:34 +0530

    feat(events): add dynamic data for cards and use interfaces

--- client/src/components/events/cards/EventCard.tsx
+++ client/src/components/events/cards/EventCard.tsx
@@ -1,23 +1,24 @@
-const EventCard = () => {
+import { Event } from "../../../types/event";
+
+const EventCard = ({ eventData }: { eventData: Event }) => {
     return (
         <div className="bg-white shadow-lg rounded-lg overflow-hidden">
             <img
                 className="w-full h-56 object-cover object-center"
-                src={"https://placehold.co/600x400?text=Hackathon"}
+                src={eventData.image}
                 alt="event"
             />
             <div className="p-4">
                 <h1 className="text-2xl font-semibold text-gray-800">
-                    {"Hackathon"}
+                    {eventData.title}
                 </h1>
                 <p className="mt-2 text-gray-600">
-                    {
-                        "A coding competition where participants showcase their skills and build innovative solutions."
-                    }
+                    {eventData.description.length > 100
+                        ? eventData.description.substring(0, 100) + "..."
+                        : eventData.description}
                 </p>
                 <div className="flex justify-between items-center mt-4">
-                    {/* <Link to={`/events/${event.name}`} className="text-blue-600 hover:underline">View</Link> */}
-                    <p className="text-gray-600">Date: {"22-09-2024"}</p>
+                    <p className="text-gray-600">Date: {eventData.date}</p>
                 </div>
             </div>
         </div>
