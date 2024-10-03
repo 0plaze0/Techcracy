b1bc8c5d49332f1ff9f01d8eaed120898da9d5c7
Abhishek Kashyap <abhik5567@gmail.com>
2024-10-04 00:58:10 +0530

    refactor: fix navbar arrows

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.scss";
-import {
-    Home,
-    EventPage,
-    AdminDashboard,
-    AllEvent,
-    AllOrganizer,TeamPage, WebTeam
-} from "./pages";
+import { Home, EventPage, TeamPage } from "./pages";
 import { Carousel3D } from "./components";
 function App() {
     return (
@@ -14,13 +8,8 @@ function App() {
             <Route path="/">
                 <Route index element={<Home />} />
                 <Route path="team" element={<TeamPage />} />
-                <Route path="web-team" element={<WebTeam />} />
                 <Route path="3d" element={<Carousel3D />} />
                 <Route path="events" element={<EventPage />} />
-                <Route path="admin" element={<AdminDashboard />}>
-                    <Route path="all-events" element={<AllEvent />} />
-                    <Route path="all-organizer" element={<AllOrganizer />} />
-                </Route>
             </Route>
         </Routes>
     );
--- client/src/components/events/eventSection/eventSection.tsx
+++ client/src/components/events/eventSection/eventSection.tsx
@@ -1,12 +1,14 @@
-import WorkshopCarousel from  "../WorkshopCarousel/WorkshopCarousel";
+import WorkshopCarousel from "../WorkshopCarousel/WorkshopCarousel";
 import { EventSectionProps } from "../../../types/event";
-export default function EventSection({ events }: { events: EventSectionProps }) {
+export default function EventSection({
+    events,
+}: {
+    events: EventSectionProps;
+}) {
     return (
         <section className="bg-gray-100 py-12">
             <div>
-                {/* <EventContainer events={events} /> */}
-
-                <h1 className="text-3xl font-semibold text-gray-800 center-align">
+                <h1 className="text-3xl font-semibold text-gray-800 text-center">
                     {events.event_type}
                 </h1>
                 <WorkshopCarousel events={events.events} />
