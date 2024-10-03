03a79222b012a0054509d1d6b5d9431e0a59d216
krishna <krishnasharmacit@gmail.com>
2024-10-03 20:18:29 +0530

    feat: add navbar

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,5 +1,6 @@
 import { Carousel3D } from "../../components";
 import { Hero, Events, Slider, Merchandise } from "../../components/home";
+import Sponsor from "../../components/home/sponsor/sponsor";
 // import Navbar from "../../components/navbar/navbar";
 
 export default function HomePage() {
@@ -12,6 +13,7 @@ export default function HomePage() {
             <Merchandise />
             <Slider />
             {/* <Slider /> */}
+            <Sponsor/>
         </main>
     );
 }
