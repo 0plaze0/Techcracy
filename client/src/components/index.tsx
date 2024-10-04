6914514de327eaca713069487923b339581c7785
Abhishek Kashyap <abhik5567@gmail.com>
2024-10-04 17:20:37 +0530

    feat: remove all unwanted dependencies

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,3 +1,2 @@
 export { default as Sidebar } from "./Sidebar";
-export { default as OrganizerIcon } from "./events/organizer/OrganizerIcon";
 export { default as Carousel3D } from "./carousel/Carousel3D";
