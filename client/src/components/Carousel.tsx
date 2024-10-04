6a5c80f9287a15c9b035f16e810b0fc198c1c39b
Abhishek Kashyap <abhik5567@gmail.com>
2024-10-04 16:02:01 +0530

    fix : remove unwanted files

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 
 const Content = styled.div`
   position: relative;
